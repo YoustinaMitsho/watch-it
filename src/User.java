a7aba01a24cbfbb88d158f131b6d871af1fbc52a
unknown <youstinamitcho@gmail.com>
2023-11-28 18:50:33 +0200

    person class (abstract) done, Admin and User classes not yet

---
+++ src/User.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class User extends personx{
+    int age; //3shan offer films related
+    String nationality;//3shan offer films related
+    String subscription_type;
+
+    String []wartchlater = new String [200];
+    String []alreadywatched  = new String [200]; // is a class to be implemented later, will have child here
+    public void chooseplan(String plantype,int basic_counter,int standard_counter, int premium_counter){
+        System.out.println("choose your plan");
+        Scanner input =new Scanner(System.in);
+        if (plantype.equals("basic")){
+            basic_counter++;
+        }
+        else if (plantype.equals("standard")) {
+            standard_counter++;
+
+        } else if (plantype.equals("premium")) {
+            premium_counter++;
+
+        }
+    }
+
+}
---
+++ src/personx.java
@@ -0,0 +1,9 @@
+public class personx {
+    int id;
+    String username;
+    int password;
+    String firstname;
+    String secondname;
+    String email;
+    static int basic_counter,standard_counter,premium_counter;
+}
